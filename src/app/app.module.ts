5a2e075f2f9a4cc9d6fc5dc03fddcff180db57a6
Sukhleen123 <58146136+Sukhleen123@users.noreply.github.com>
2020-08-19 23:16:13 -0400

    initial version template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconComponent } from './icon/icon.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import {FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './home';
+import { AboutComponent } from './about';
+import { MissionComponent } from './mission';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    MissionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/donate/donate.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({ 
+    templateUrl: 'donate.component.html'
+})
+export class DonateComponent {}
